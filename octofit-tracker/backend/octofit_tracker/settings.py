95c0265ea0dfffbf687eee16add98b0291fd48f3
Oswaldo Olivo <oswaldo.l.olivo@gmail.com>
2026-04-18 21:28:46 +0000

    REST API creation

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -25,7 +25,12 @@ SECRET_KEY = 'django-insecure-4ct^%p^3vd*w+0x=b#8=ios=4k-aqq#t7l3$rljc#@2q0xl3-t
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+import os
+codespace_name = os.environ.get('CODESPACE_NAME')
+allowed_hosts = ['localhost', '127.0.0.1']
+if codespace_name:
+    allowed_hosts.append(f"{codespace_name}-8000.app.github.dev")
+ALLOWED_HOSTS = allowed_hosts
 
 
 # Application definition
